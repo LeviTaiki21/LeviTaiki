0d3a0e66c551e5f46f49eae01596b78d18072839
LeviTaiki21 <levitaiki791@gmail.com>
2024-11-06 22:56:00 +0800

    Add files via upload

---
+++ Liwagon,LeviLloyd/average.js
@@ -0,0 +1,6 @@
+const x = parseInt (prompt("Enter x:"))
+const y = parseInt (prompt("Enter y:"))
+
+const average = (x + y) / 2;
+
+console.log("Average: " + average.toFixed(2));
---
+++ Liwagon,LeviLloyd/gpa,height,weight.js
@@ -0,0 +1,8 @@
+let gpa = parseFloat(prompt("Enter your GPA:"));
+let height = parseFloat(prompt("Enter your height (in meters):"));
+let weight = parseFloat(prompt("Enter your weight (in kilograms):"));
+
+
+console.log("GPA: " + gpa.toFixed(2));
+console.log("Height: " + height.toFixed(2));
+console.log("Weight: " + weight.toFixed(2));
